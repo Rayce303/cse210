43f1bf86724423fb65c1e33bd37099953b8ac6c0
Rayce303 <orullianrayce@gmail.com>
2026-09-21 12:05:00 -0600

    Week 2 Prep Activities
    I did the James Bond activity and the IF statement practice.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letter;
+
+        if (grade <= 100 && grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade <= 90 && grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade <= 80 && grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade <= 70 && grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Yay! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Oh no, do better next time!");
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Boof boof McGoof");
+        int x = 8;
+        int y = 14;
+        int z = 40;
+
+        if (x == 10 && y == 12 || z == 40)
+        {
+            Console.WriteLine("X is 10");
+            Console.WriteLine("waffle");
+        }
+        else if (x == 20)
+        {
+            Console.WriteLine("Rawr");
+        }
+        else
+        {
+            Console.WriteLine("sneep snoop");
+        }
     }
 }

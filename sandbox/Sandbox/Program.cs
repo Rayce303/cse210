b90f6b2be8a83bc876b72a707ed114a8d5f171ae
Rayce303 <orullianrayce@gmail.com>
2026-09-23 11:31:52 -0600

    Learned some loops
    Yeah I did loops in the sandbox.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,22 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        int x = 8;
-        int y = 14;
-        int z = 40;
+    //     int x = 8;
+    //     int y = 14;
+    //     int z = 40;
 
-        if (x == 10 && y == 12 || z == 40)
+    //     if (x == 10 && y == 12 || z == 40)
+    //     {
+    //         Console.WriteLine("X is 10");
+    //         Console.WriteLine("waffle");
+    //     }
+    //     else if (x == 20)
+    //     {
+    //         Console.WriteLine("Rawr");
+    //     }
+    //     else
+    //     {
+    //         Console.WriteLine("sneep snoop");
+    //     }
+    // }
+
+        // bool done = false;
+
+        // while (! done)
+        // {
+        //     Console.Write("Am I done yet? ");
+        //     done = Console.ReadLine().ToLower() == "y";
+        // }
+
+        // bool done;
+
+        // do
+        // {
+        //     Console.Write("Am I done yet? ");
+        //     done = Console.ReadLine().ToLower() == "y";
+        // } while (! done);
+
+        // for (double i = 1000.0; i > 0; i -= 12.18)
+        // {
+        //     Console.WriteLine(i);
+        // }
+
+        // List<string> myFriends = new List<string>("Larry", "Daryll", "Daryll");
+        // List<string> myFriends2 = ["Larry", "Daryll", "Daryll"];
+        // myFriends.Add("Jimmy Wimmy");
+
+        static double AddThemNumbers(double x, int y)
         {
-            Console.WriteLine("X is 10");
-            Console.WriteLine("waffle");
+            return x + y;
         }
-        else if (x == 20)
-        {
-            Console.WriteLine("Rawr");
-        }
-        else
-        {
-            Console.WriteLine("sneep snoop");
-        }
+
+        AddThemNumbers(5, 3);
     }
 }
